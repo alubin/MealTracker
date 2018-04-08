ceac52c27ddb4d6e030b0e2aee40554b073808af
Andy Lubin <andylubin@gmail.com>
2018-04-08 14:26:53 -0700

    Clean up Javascript to better match standards

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,80 +1,83 @@
 import React from 'react';
-import {ScrollView, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
 import _ from 'lodash';
-import styles from "../../styles";
-import Meal from "./Meal";
+import styles from '../../styles';
+import Meal from './Meal';
 
 const placeHolder = '>Meal';
 const headerText = 'Meal Tracker';
 
 export default class Main extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mealArray: [],
-            mealText: '',
-        }
+  constructor (props) {
+    super(props);
+    this.state = {
+      mealArray: [],
+      mealText: ''
+    };
 
-        this.addButton = this.addNote.bind(this);
+    this.addButton = this.addNote.bind(this);
+  }
+
+  render () {
+    let meals = this.state.mealArray.map((val, key) => {
+      return (
+        <Meal
+          key={key}
+          keyVal={key}
+          meal={val}
+          deleteMethod={() => this.deleteMeal(key)}
+        />
+      );
+    });
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text>{headerText}</Text>
+        </View>
+
+        <ScrollView style={styles.scrollContainer}>{meals}</ScrollView>
+
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={mealText => this.setState({ mealText: mealText })}
+            value={this.state.mealText}
+            placeholder={placeHolder}
+            placeholderTextColor={'white'}
+            underlineColorAndroid={'transparent'}
+          />
+
+          <TouchableOpacity onPress={this.addButton} style={styles.addButton}>
+            <Text style={styles.addButtonText}>+</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  addNote () {
+    if (!_.isEmpty(this.state.mealText)) {
+      const d = new Date();
+      const meals = this.state.mealArray;
+      meals.push({
+        date: d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate(),
+        meal: this.state.mealText
+      });
+      this.setState({ mealArray: meals });
+      // Clears input box, for next input
+      this.setState({ mealText: '' });
     }
+  }
 
-    render() {
-
-        let meals = this.state.mealArray.map((val, key) => {
-            return <Meal key={key}
-                         keyVal={key}
-                         meal={val}
-                         deleteMethod={() => this.deleteMeal(key)}/>
-        });
-
-        return (
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <Text>{headerText}</Text>
-                </View>
-
-                <ScrollView style={styles.scrollContainer}>
-                    {meals}
-                </ScrollView>
-
-                <View style={styles.footer}>
-                    <TextInput style={styles.textInput}
-                               onChangeText={(mealText) => this.setState({mealText: mealText})}
-                               value={this.state.mealText}
-                               placeholder={placeHolder}
-                               placeholderTextColor={'white'}
-                               underlineColorAndroid={'transparent'}>
-                    </TextInput>
-
-                    <TouchableOpacity onPress={this.addButton} style={styles.addButton}>
-                        <Text style={styles.addButtonText}>
-                            +
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        )
-    }
-
-    addNote() {
-        if(!_.isEmpty(this.state.mealText))
-        {
-            const d = new Date();
-            const meals = this.state.mealArray;
-            meals.push({
-                date: d.getFullYear() +
-                    '/' + (d.getMonth() + 1) +
-                    '/' + d.getDate(),
-                meal: this.state.mealText
-                });
-            this.setState({mealArray: meals});
-            //Clears input box, for next input
-            this.setState({mealText: ''});
-        }
-    }
-
-    deleteMeal(key){
-        this.state.mealArray.splice(key, 1);
-        this.setState({mealArray: this.state.mealArray});
-    }
+  deleteMeal (key) {
+    this.state.mealArray.splice(key, 1);
+    this.setState({ mealArray: this.state.mealArray });
+  }
 }
--- app/components/Meal.js
+++ app/components/Meal.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
-import styles from "../../styles";
+import { Text, TouchableOpacity, View } from 'react-native';
+import styles from '../../styles';
 import PropTypes from 'prop-types';
 
 export default class Meal extends React.Component {
-    render() {
-        return (
-            <View key={this.props.keyVal} style={styles.meal}>
-                <Text style={styles.mealText}>{this.props.meal.date}</Text>
-                <Text style={styles.mealText}>{this.props.meal.meal}</Text>
+  render () {
+    return (
+      <View key={this.props.keyVal} style={styles.meal}>
+        <Text style={styles.mealText}>{this.props.meal.date}</Text>
+        <Text style={styles.mealText}>{this.props.meal.meal}</Text>
 
-                <TouchableOpacity onPress={this.props.deleteMethod} style={styles.mealDelete}>
-                    <Text style={styles.deletionText}>D</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
+        <TouchableOpacity
+          onPress={this.props.deleteMethod}
+          style={styles.mealDelete}
+        >
+          <Text style={styles.deletionText}>D</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
-//Not needed, but want to ensure I get what I expect.
+// Not needed, but want to ensure I get what I expect.
 Meal.propTypes = {
-    keyVal: PropTypes.number,
-    deleteMethod: PropTypes.func.isRequired,
-    meal: PropTypes.object
-}
+  keyVal: PropTypes.number,
+  deleteMethod: PropTypes.func.isRequired,
+  meal: PropTypes.object
+};
